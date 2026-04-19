fef0bd52fae6475f02a75d02d4e38ef7c60d6e14
Krish Garg <er.krishgarg@gmail.com>
2026-04-19 21:08:49 +0530

    Delete interfaces.java

--- interfaces.java
+++
@@ -1,9 +0,0 @@
-package smartwaste;
-
-interface Reportable {
-    void generateSummary();
-}
-
-interface AlertSystem {
-    void triggerAlert(String message, String severity); // severity: low, moderate, high, critical
-}
